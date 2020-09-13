2a5a3a36a9dc3656c04ea4c123e429a7acb47218
Derek Williamson <derekwilliamson22@gmail.com>
2020-09-12 19:47:49 -0500

    copying previous into new repo started from template not fork

---
+++ server/server.js
@@ -0,0 +1,17 @@
+// requires
+const express = require( 'express' );
+const app = express();
+const bodyParser = require( 'body-parser' );
+const tasks = require( './routes/tasks.route' );
+// uses
+app.use( express.static( 'server/public' ) );
+app.use( bodyParser.urlencoded( { extended: true } ) );
+app.use( '/tasks', tasks );
+
+// globals
+const port = 3000;
+
+// spin up server
+app.listen( port, ()=>{
+    console.log( 'standing by on', port );
+}) // end server up
