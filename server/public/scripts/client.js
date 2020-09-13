01ed64738954fb9283a4d89c3cda93aedebc2d40
Derek Williamson <derekwilliamson22@gmail.com>
2020-09-13 00:28:32 -0500

    troubleshooting a 404 error-dont understand the source of the problem

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,9 +1,10 @@
-$( document ).ready( onReady );
+$(document).ready(onReady);
 
 function onReady(){
     $(document).on('click', '#taskSubmitBtn', addTask);
     getTasks();
 } // end onReady
+
 
 function addTask(){
     console.log('in addTask');
@@ -11,42 +12,45 @@ function addTask(){
         task_name: $('#taskNameIn').val(),
         task_desc: $('#taskDescIn').val(),
         task_time: $('#taskTimeIn').val()
-    }
-    console.log('sending this:', objectToSend);
+    };
     $.ajax({
         method: 'POST',
         url: '/tasks',
         data: objectToSend
-    }).then(function(response){
+    })
+    .then(function(response){
         console.log('back from POST:', response);
         getTasks();
-    }).catch(function(error){
-        console.log(error);
-        alert('somethings amiss')
     })
-} // end addMovie
+    .catch(function(err){
+        alert('somethings amiss');
+        console.log(err);
+    });
+} // end addTask
 
 function getTasks(){
     console.log('in getTasks');
     $.ajax({
         method: 'GET',
         url: '/tasks'
-    }).then(function(response){
+    })
+    .then(function(response){
         let el = $('.taskDisplay');
         el.empty();
         for(let i=0; i< response.length; i++){
             let task = response[i];
             el.append(`
-            <div class="taskOut" data-id="${task.id}>
+            <div class="taskOut" data-id="${task.id}">
             <h2 class="taskNameTag">${task.task_name}</h2>
             <p class="taskDescTag">${task.task_desc}</p>
-            <p class="taskTimeTag">Estimated time to complete:\n
+            <p class="taskTimeTag">Estimated time to complete:
             ${task.task_time}
             </div>`)
         }
-    }).catch(function(error){
-        console.log(error);
-        alert('somethings amiss')
     })
+    .catch(function(error){
+        alert('somethings amiss');
+        console.log(error);
+    });
 } // end getTasks
 
--- server/server.js
+++ server/server.js
@@ -2,16 +2,57 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const tasks = require('./routes/tasks.route');
+const pg = require( 'pg' );
+
 // uses
-app.use( express.static('server/public'));
-app.use( bodyParser.urlencoded({extended: true}));
-app.use('/tasks', tasks);
+app.use(express.static('server/public'));
+app.use(bodyParser.urlencoded({extended: true}));
 
 // globals
-const port = 3000;
+const port = 3001;
+
+const Pool = pg.Pool;
+const pool = new Pool({
+    database: 'weekend-to-do-app',
+    host: 'localhost',
+    port: 5432,
+    max: 12,
+    idleTimeoutMillis: 40000
+})
 
 // spin up server
 app.listen( port, ()=>{
     console.log('standing by on', port);
 }) // end server up
+
+app.get('/tasks', (req, res)=>{
+    console.log('/tasks GET hit');
+    const queryString = `SELECT * FROM "tasks"`;
+    pool
+    .query(queryString)
+    .then((results)=>{
+        res.send(results.rows);
+    })
+    .catch((err)=>{
+    console.log('something amiss in task get', err);
+    res.sendStatus(500);
+    });
+}); // end /tasks GET
+
+app.post('/tasks', (req, res)=>{
+    console.log('/tasks POST hit:', req.body);
+    const queryString = `INSERT INTO "tasks" (task_name, task_desc, task_time) VALUES ($1, $2, $3)`;
+    pool
+    .query(queryString, [
+        req.body.task_name,
+        req.body.task_desc,
+        req.body.task_time
+    ])
+    .then((results)=>{
+        res.sendStatus(201);
+    })
+    .catch((err)=>{
+        console.log('somethings amiss with task add', err);
+        res.sendStatus(500);
+    });
+}); // end POST
